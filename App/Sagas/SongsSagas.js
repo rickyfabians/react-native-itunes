f26dfba066278d06e1ca87e7039e0eae2026289a
ricky <rickyfabian999@gmail.com>
2021-04-11 23:12:00 +0700

    fix: remove console from songsSagas

--- App/Sagas/SongsSagas.js
+++ App/Sagas/SongsSagas.js
@@ -6,7 +6,6 @@ export function * searchSong (api, { keyword }) {
   const response = yield call(api.searchSong, keyword)
   if (response.ok) {
     // do data conversion here if needed
-    console.log(JSON.stringify(response.data))
     yield put(SongsActions.success(response.data))
   } else {
     yield put(SongsActions.failure())
